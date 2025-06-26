74ef8d2e8e5278fe5d0d76b5149e625aff2b2e0c
gomkyung2 <stripe2933@gmail.com>
2025-06-26 17:36:59 +0900

    Added flag-enum port.

---
+++ ports/flag-enum/portfile.cmake
@@ -0,0 +1,26 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO stripe2933/flag_enum
+    REF "v${VERSION}"
+    HEAD_REF main
+    SHA512 d3b9c421df5b22206556ceb61c7f962aae9d1cba19239402ef87260598bb9e28528a9c8e9afa56a786844d32257c4f22de100d631c3c5a7d759d9ed114b0cf9f
+)
+
+set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
+vcpkg_cmake_install()
+
+if (NOT VCPKG_BUILD_TYPE STREQUAL "release")
+    file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/debug/share)
+    file(RENAME ${CURRENT_PACKAGES_DIR}/debug/cmake/flag_enum ${CURRENT_PACKAGES_DIR}/debug/share/flag_enum)
+endif()
+if (NOT VCPKG_BUILD_TYPE STREQUAL "debug")
+    file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/share)
+    file(RENAME ${CURRENT_PACKAGES_DIR}/cmake/flag_enum ${CURRENT_PACKAGES_DIR}/share/flag_enum)
+endif()
+
+vcpkg_cmake_config_fixup(PACKAGE_NAME "flag_enum")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/cmake" "${CURRENT_PACKAGES_DIR}/debug/cmake")
+
+file(INSTALL "${SOURCE_PATH}/LICENSE.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
