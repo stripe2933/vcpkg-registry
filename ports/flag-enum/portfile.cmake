a142b393bc3fbbc65e6a5b8e8b4604afc940e81b
gomkyung2 <stripe2933@gmail.com>
2025-06-26 17:40:17 +0900

    Fixed flag-enum portfile.cmake.

--- ports/flag-enum/portfile.cmake
+++ ports/flag-enum/portfile.cmake
@@ -7,6 +7,8 @@ vcpkg_from_github(
 )
 
 set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
+
+vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 vcpkg_cmake_install()
 
 if (NOT VCPKG_BUILD_TYPE STREQUAL "release")
